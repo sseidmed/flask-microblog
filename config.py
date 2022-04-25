6fc26cb1ba9a0e79cdb061c2ad883d3973e6a99f
Shay <sseidmed@gmail.com>
2022-04-25 13:10:57 -0400

    Complete database chapter

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
 import os
 
+from flask_sqlalchemy import SQLAlchemy
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config(object):
     SECRET_KEY  = os.environ.get('SECRET_KEY') or 'super-secret-key-here'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
